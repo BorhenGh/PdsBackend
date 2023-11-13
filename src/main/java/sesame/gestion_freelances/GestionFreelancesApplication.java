5f9b117f392a59a58a889e04580d1633e1b001d2
borhen.ghrairi <borhenghrairi7@gmail.com>
2023-11-13 22:30:53 +0100

    Lancement de dev BackEnd

---
+++ src/main/java/sesame/gestion_freelances/GestionFreelancesApplication.java
@@ -0,0 +1,13 @@
+package sesame.gestion_freelances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFreelancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFreelancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sesame/gestion_freelances/GestionFreelancesApplicationTests.java
@@ -0,0 +1,13 @@
+package sesame.gestion_freelances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFreelancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
